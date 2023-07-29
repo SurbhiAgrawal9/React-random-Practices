a8c9f52c1d407bdbae3f11cbb6cbe28d637e3828
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-07-29 19:07:37 +0530

    Create Toggle the Data.js

---
+++ Toggle the Data.js
@@ -0,0 +1,17 @@
+//App.js
+
+import { uneState, useState } from "react";
+
+export default function App() {
+  const [myName, setMyName] = useState("Surbhi");
+  const ChangeName = () => {
+    let newOne = myName;
+    newOne === "Surbhi" ? setMyName("I am the best ...") : setMyName("Surbhi");
+  };
+  return (
+    <div className="App">
+      <h1>{myName} </h1>
+      <button onClick={ChangeName}>Click me</button>
+    </div>
+  );
+}
